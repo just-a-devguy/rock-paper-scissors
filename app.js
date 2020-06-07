065af221e1de4e1ffd3923f199de522ea4b207a5
Adler Luders <adlerluders@gmail.com>
2020-06-07 12:46:56 -0400

    finalized overall logic of game and worked on mobile design

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const rock = document.getElementById("rock");
-paper = document.getElementById("paper");
-scissor = document.getElementById("scissor");
-replay = document.getElementById("replay");
-scoreValue = document.getElementById("score");
-results = document.getElementById("results");
-choices = document.getElementById("choices");
-userRes = document.getElementById("user");
-cpuRes = document.getElementById("cpu");
-resultMssg = document.getElementById("mssg");
-
-options = [rock, paper, scissor];
-cpuOptions = ["rock", "paper", "scissor"];
+const rock = document.getElementById("rock"),
+  paper = document.getElementById("paper"),
+  scissor = document.getElementById("scissor"),
+  replay = document.getElementById("replay"),
+  scoreValue = document.getElementById("score"),
+  results = document.querySelector(".results-display"),
+  choices = document.querySelector(".user-choices"),
+  userRes = document.getElementById("user"),
+  cpuRes = document.getElementById("cpu"),
+  decision_mssg = document.getElementById("decision-mssg"),
+  resultMssg = document.getElementById("mssg"),
+  options = [rock, paper, scissor],
+  cpuOptions = ["rock", "paper", "scissor"];
 
 options.forEach((op) => {
   op.addEventListener("click", playGame);
@@ -34,18 +34,18 @@ function playGame() {
   if (user === "rock" && cpu === "paper") {
     score--;
     resultMssg.innerHTML = "Lose";
-  } else if (user === "rock" && cpu === "scissor") {
-    score++;
-    resultMssg.innerHTML = "Win";
-  } else if (user === "paper" && cpu === "rock") {
-    score++;
-    resultMssg.innerHTML = "Win";
   } else if (user === "paper" && cpu === "scissor") {
     score--;
     resultMssg.innerHTML = "Lose";
   } else if (user === "scissor" && cpu === "rock") {
     score--;
     resultMssg.innerHTML = "Lose";
+  } else if (user === "rock" && cpu === "scissor") {
+    score++;
+    resultMssg.innerHTML = "Win";
+  } else if (user === "paper" && cpu === "rock") {
+    score++;
+    resultMssg.innerHTML = "Win";
   } else if (user === "scissor" && cpu === "paper") {
     score++;
     resultMssg.innerHTML = "Win";
@@ -60,17 +60,24 @@ function resultPage() {
   choices.style.display = "none";
   results.style.display = "block";
 
-  userRes.innerHTML = user;
+  userRes.classList.add(user);
 
   setTimeout(() => {
-    cpuRes.innerHTML = cpu;
+    cpuRes.classList.add(cpu);
   }, 1000);
+
+  setTimeout(() => {
+    decision_mssg.style.display = "flex";
+  }, 1500);
 }
 
 function resetGame() {
   user = "";
   cpu = "";
   scoreValue.innerHTML = score;
+
+  cpuRes.className = "choice-item";
+  userRes.className = "choice-item";
 
   choices.style.display = "block";
   results.style.display = "none";
